f7b461dc343e3dcbe92b73c751e39fce3faf6631
faridulhaque <faridulhaquemurshed@gmail.com>
2022-06-07 17:50:59 +0600

    small changes

--- src/components/Supplier/Supplier.js
+++ src/components/Supplier/Supplier.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Supplier = () => {
     return (
         <div className="text-center" style={{height: '100vh'}}>
-            <h1 className="text-center mt-5">Eid Mubarak <br/> ঈদ মোবারক</h1>
+            
             this page is under construction
         </div>
     );
